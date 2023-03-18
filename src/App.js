4cde0fec48cc7f5fb8c7c0db6c453c1f62b0d605
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-03-18 22:53:26 +0100

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Timer from "./components/Timer/Timer";
+import './styles/global.scss';
+import { useEffect } from "react";
 
-function App() {
+
+const App = () => {
+
+  const [time, setTime] = useState(0);
+
+  
+
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    let timeGo; //zmienna która przechowuje wartośći setInterval z hooka useEffect() w zależności od stanu isRunning (czyli czy stoper jest uruchomiony czy nie)
+
+    if (intervalId) {
+      timeGo = setInterval(() => setTime((prevValue) => prevValue + 10), 10); //Za pomocą hooka useEffect() definiowana jest funkcja, która co 10ms zwiększa wartość stanu time o 10
+    }
+    return () => clearInterval(timeGo); //Za pomocą hooka useEffect() definiowana jest funkcja, która po zatrzymaniu stopera czyści setInterval
+  }, [intervalId]);
+  
+
+  const startTimer = () => {
+    
+      setIntervalId(true)
+      
+    };
+  
+  const stopTimer = () => {
+    
+    setIntervalId(null);
+  }
+
+  const resetTime =()=> {
+    setTime(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+       <Timer  time ={time} />
+       <div className='buttons'>
+        <button className='button'onClick={startTimer}>Start</button>
+        <button className='button'onClick={stopTimer}>Stop</button>
+        <button className='button'onClick={resetTime}>Reset</button>
+      </div>
     </div>
+    
   );
 }
 
